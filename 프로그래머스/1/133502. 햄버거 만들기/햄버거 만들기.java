c496d39ebb87f97cc8341d6235df347c94e9bf0f
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2024-01-01 23:06:38 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,23 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
-       int answer = 0;
-		List<Integer> li = new ArrayList<>();
-		
-		for(int i :ingredient) {
-			li.add(i);
-			while(li.size() >= 4) {
-				int n = li.size();
-				if(!(li.get(n-1) == 1
-					&& li.get(n-2)==3
-					&& li.get(n-3) ==2
-					&& li.get(n-4)==1)) break;
-				for(int j=0; j<4; j++) {
-					li.remove(li.size() -1);
-				}
-				answer++;
-			}
-		}
+        
+        int answer = 0;
+        ArrayList<String> str = new ArrayList<String>();
+        String strArr = "";
+        
+        for(int i = 0; i < ingredient.length; i ++) {
+            switch(ingredient[i]) {
+                case 1 : str.add("빵");   break;
+                case 2 : str.add("야채"); break;
+                case 3 : str.add("고기"); break;
+            }
+        }
+        
+        ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
+
+        for(int i = 0; i <= (str.size() - 4); i++) { 
+            arr.add(new ArrayList<String>()); // ArrayList<String>를 초기화하고 추가
+            
+            for(int j = i; j < (i + 4); j++) {
+                arr.get(i).add(str.get(j));
+            }
+            
+            strArr = "";
+            for(int a = 0; a < 4; a++) {
+                strArr += arr.get(i).get(a);   
+                if(strArr.equals("빵야채고기빵")) {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
+       
     }
 }
