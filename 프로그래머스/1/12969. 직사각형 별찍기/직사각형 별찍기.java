1126365b81be385a8e020e08eb7375426506f43e
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2024-02-11 12:23:05 +0900

    [level 1] Title: 직사각형 별찍기, Time: 194.12 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        int b = sc.nextInt();
+
+        for(int i = 0; i < (a*b); i++) {
+            if(i % a ==0 && i != 0) {
+                System.out.println();
+            }
+            System.out.print("*");
+            
+        }
+
+    }
+}
