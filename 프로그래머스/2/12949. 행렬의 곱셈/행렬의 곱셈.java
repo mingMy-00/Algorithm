e0be5a563c6a6de5a74a6e86296d1c8e2f1f4fdd
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2024-07-21 11:26:14 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.38 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        //완전탐색 같긴함  !! 
+    
+    //1. 변수명 생성 
+    int le1 = arr1.length;    //행의 길이 만큼 반복
+    int le2 = arr1[0].length; //행의 내재 수들 반복
+    int le3 = arr2.length;    //열의 길이 만큼 안쪽 반복
+    int le4 = arr2[0].length; //열의 내재 길이 만큼 곱하기 
+        
+    //2. 사이즈 할당     
+    int[][] answer = new int[le1][le4];
+
+    //3. 곱해주면서 값 더해주기 
+    //3_1. 전체 곱의 수는 행의 길이 만큼  (3번)
+    for(int i = 0 ; i < le1; i++) { 
+    //3_2. 열의 길이 만큼 다시 곱해줌  (3*2)
+        for(int j = 0; j < le4; j++) { 
+    //3_3. 3*2안에서 2*2가 되어야함 (arr1[0] * arr2[0]) = (le2 * le3)
+            for(int a = 0; a < le2; a++) {
+              answer[i][j] += arr1[i][a] * arr2[a][j];    
+            }
+        }
+    }
+        
+        return answer;
+    }
+}
