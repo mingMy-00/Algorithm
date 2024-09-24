2703b60997eceaf7ff4e63025bc002060141020b
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2024-09-24 22:56:58 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1060 ms, Memory: 55016 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] agrs) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    HashMap<Integer, String> map1 = new HashMap<>();
+    HashMap<String, Integer> map2 = new HashMap<>();
+
+    for(int i = 1; i <= N; i++) {
+      String s = br.readLine();
+      map1.put(i, s);
+      map2.put(s, i);
+    }
+
+    for(int i = 0; i < M; i++) {
+          String s = br.readLine();
+          char c = s.charAt(0);
+      if(Character.isDigit(c)) {
+          System.out.println(map1.get(Integer.parseInt(s)));
+      }else {
+          System.out.println(map2.get(s));
+      }
+    }
+  }
+}
