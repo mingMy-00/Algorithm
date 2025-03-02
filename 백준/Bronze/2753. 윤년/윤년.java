aa2ce615040d89cee60697482f69893ab5b5e6fd
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2025-03-02 15:52:20 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        
+        if(A % 4 == 0 && A % 100 != 0 || A % 400 == 0) {
+            System.out.println(1);
+        }else {
+            System.out.println(0);
+        }
+    }
+}
