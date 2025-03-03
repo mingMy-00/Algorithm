a66dafa56c5f4d135d5e709516b66c156b6b0466
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2025-03-04 01:24:56 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 16144 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        String[] arr = new String[2];
+        
+        for(int i =1; i <=T; i++) {
+            arr = br.readLine().split(" ");
+            System.out.println("Case #" + i + ": " + (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1])));
+        }
+    }
+}
