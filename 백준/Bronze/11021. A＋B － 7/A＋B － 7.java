d0001f98877335bbc9053352e94c8c2a16fd676f
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2025-03-04 01:25:39 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
