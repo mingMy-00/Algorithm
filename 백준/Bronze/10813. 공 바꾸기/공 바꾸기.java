971dc9a58804c77c35d25fe4dee5b3859477b432
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2025-03-24 16:56:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -6,21 +6,19 @@ public class Main {
         String BasketArr[] = br.readLine().split(" ");
         int N = Integer.parseInt(BasketArr[0]);
         int M = Integer.parseInt(BasketArr[1]);
-        int num1 = 0;
-        int num2 = 0;
         
         int basket[] = new int[N];
-        
         for(int a = 1; a <= N; a++) {
             basket[a-1] = a;
         }
        
         for(int k = 0; k < M; k ++) {
             BasketArr = br.readLine().split(" ");
-            num1 = basket[Integer.parseInt(BasketArr[0]) - 1];
-            num2 = basket[Integer.parseInt(BasketArr[1]) - 1 ];
-            basket[Integer.parseInt(BasketArr[0]) - 1] = num2;
-            basket[Integer.parseInt(BasketArr[1]) - 1] = num1;
+            int num1 = Integer.parseInt(BasketArr[0]) - 1;
+            int num2 = Integer.parseInt(BasketArr[1]) - 1;
+            int temp = basket[num1];
+            basket[num1] = basket[num2];
+            basket[num2] = temp;
         }
       
         for(int arr : basket) {
