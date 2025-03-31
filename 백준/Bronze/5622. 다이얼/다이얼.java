f20202be2671a15361ec902dd983fbfa23607b28
Minje Kim <149574203+mingMy-00@users.noreply.github.com>
2025-03-31 10:04:16 +0900

    [Bronze II] Title: 다이얼, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,40 +1,30 @@
 import java.io.*;
 public class Main {
 		public static void main(String[] args) throws IOException {
-			//1 -> 2초
-			//2 -> 3초 abc
-			//... 9까지
-			//일단 전화기를 구현하고
-			//변수 설정
-			int count = 0;
-			//1. 일반 리스트 배열을 10개 만들고 그 안에 2번방 부터 abc, def, ghi, jkl, mno, pqrs, tuv, wxyz를 넣는다.
-			String phoneDial[] = new String[10];
-			//2. 1번방에는 아무것도 안 넣어도 됨.
-			phoneDial[0] = "";
-			phoneDial[1] = "";
-			phoneDial[2] = "ABC";
-			phoneDial[3] = "DEF";
-			phoneDial[4] = "GHI";
-			phoneDial[5] = "JKL";
-			phoneDial[6] = "MNO";
-			phoneDial[7] = "PQRS";
-			phoneDial[8] = "TUV";
-			phoneDial[9] = "WXYZ";
-
-			//3. 그리고 입력받은 문자열을 charAt 해서 그 값을 기반으로 List 방 안에 있는 문자열 중 charAt으로 일치하는 값이 있는지 봄.
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String s = br.readLine();
-			for(int i = 0; i < s.length(); i++) {
-				for(int j = 0; j < phoneDial.length; j++) {
-					if(phoneDial[j].contains(String.valueOf(s.charAt(i)))) {
-						count += j + 1;
-					}
+			int count = 0;
+
+			for(char c : s.toCharArray()) {
+				if(c >= 'A' && c <=  'C') {
+					count += 3;
+				}else if(c >= 'D' && c <= 'F') {
+					count += 4;
+				}else if(c >= 'G' && c <= 'I') {
+					count += 5;
+				}else if(c >= 'J' && c <= 'L') {
+					count += 6;
+				}else if(c >= 'M' && c <= 'O') {
+					count += 7;
+				} else if (c >= 'P' && c <= 'S') {
+					count += 8;
+				} else if (c >= 'T' && c <= 'V') {
+					count += 9;
+				} else if(c >= 'W' && c <= 'Z'){
+					count += 10;
 				}
-			}
-			//4. 일치하면 해당 방 값 + 1의 시간을 더해준다.
-			//입력받아 해결
-			System.out.println(count);
-
-
-		}
+		    }
+            
+            System.out.println(count);
 	}
+}
