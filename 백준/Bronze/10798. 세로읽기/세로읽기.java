bae130fbcee6fdd5ead46779e2c06e418dc4e196
Minje Kim <wbsebqb1118@naver.com>
2025-08-05 23:06:08 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,19 +1,22 @@
 import java.io.*;
+import java.util.*;
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[][] arr = new char[5][];
         
-        int maxLen = 0;
-        for (int i = 0; i < 5; i++) {
-            arr[i] = br.readLine().toCharArray();
-            if(arr[i].length > maxLen) maxLen = arr[i].length;
+        char[][] charArr = new char[5][];
+        int maxLength = 0;
+        for(int i = 0; i < 5; i++) {
+            charArr[i] = br.readLine().toCharArray();
+            if(charArr[i].length > maxLength) {
+                maxLength = charArr[i].length;
+            }
         }
         
-        for(int j = 0; j < maxLen; j++) {
-            for(int i = 0; i < 5; i++) {
-                if(j < arr[i].length) {
-                    System.out.print(arr[i][j]);
+        for(int i = 0; i < maxLength; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(i < charArr[j].length) {
+                    System.out.print(charArr[j][i]);
                 }
             }
         }
