9b901cd9449ebf8a82aa2a0152954e617c66b1f6
Minje Kim <wbsebqb1118@naver.com>
2025-08-07 21:14:51 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 212 ms, Memory: 22960 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,55 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-        public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        Stack<Character> st = new Stack<>();
+        String str = br.readLine();
+        Stack<Character> st = new Stack();
         StringBuilder sb = new StringBuilder();
-        boolean flag = true;
-
-        //<ab cd>fe hg<ij kl>
-        //noojkeab enilno egduj
-
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
+        boolean isTag = false;
+        for(int i =0; i < str.length(); i++) {
+            
+            char c = str.charAt(i);
+            
             if(c == '<') {
-                flag = false; 
-
-                    if(!st.isEmpty()) {
-                        while(!st.isEmpty()) {
-                            sb.append(st.pop());
-                        }
-                    }
-                sb.append(c);
-                
-            }else if (c == '>') {
-                flag = true;
-                sb.append(c);
-            }else if(!flag) {
-                sb.append(c);
-            }else {
-                if(c == ' ') {
+                isTag = true;
+                if(!st.isEmpty()) {
                     while(!st.isEmpty()) {
                         sb.append(st.pop());
                     }
+                }
+                sb.append(c);
+            }else if(c =='>') {
+                isTag = false;
+                sb.append(c);
+            }else if(c == ' ') {
+                while(!st.isEmpty()) {
+                      sb.append(st.pop());
+                }
+                sb.append(c);
+            }else {
+                if(isTag) {
                     sb.append(c);
                 }else {
-                    st.push(c);    
+                    st.push(c);
                 }
             }
         }
-
+        
         if(!st.isEmpty()) {
             while(!st.isEmpty()) {
                 sb.append(st.pop());
             }
         }
-        System.out.println(sb);
-        br.close();
+        
+       System.out.println(sb);
+  
     }
+    
 }
