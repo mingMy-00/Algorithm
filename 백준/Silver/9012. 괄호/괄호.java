33b9e9c12e20fa552394015fd8c063312fdf9054
Minje Kim <wbsebqb1118@naver.com>
2025-08-23 20:10:55 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,39 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-
-    int T = sc.nextInt();
-
-    for (int i = 0; i < T; i++) {
-      System.out.println(solve(sc.next()));
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < t; i++) {
+            String str = br.readLine();
+            Stack st = new Stack();
+            boolean flag = true;
+            for(int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if(c =='(') {
+                    st.push(c);
+                }else if(c == ')') {
+                    if(!st.isEmpty() ){
+                         st.pop();
+                    }else {
+                        flag = false;
+                    }
+                }
+            }
+            
+            if(!st.isEmpty() || !flag) {
+                System.out.println("NO");
+            }else {
+                System.out.println("YES");
+            }
+            
+        }
+        
     }
-  }
-
-  private static String solve(String s) {
-
-    Stack<Character> st = new Stack<>();
-
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-
-      if (c == '(') {
-        st.push(c);
-      } else if (st.isEmpty()) {
-        return "NO";
-      } else {
-        st.pop();
-      }
-    }
-
-    if (st.isEmpty()) {
-      return "YES";
-    } else {
-      return "NO";
-    }
-  }
- }
+}
