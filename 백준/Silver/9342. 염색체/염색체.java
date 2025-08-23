29713fc4044661637f9dc4cc38973c03050edac4
Minje Kim <wbsebqb1118@naver.com>
2025-08-23 19:54:51 +0900

    [Silver III] Title: 염색체, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/9342. 염색체/염색체.java
+++ 백준/Silver/9342. 염색체/염색체.java
@@ -1,22 +1,22 @@
 import java.io.*;
 import java.util.regex.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException{
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         Pattern pattern = Pattern.compile("^[A-F]?A+F+C+[A-F]?$");
-
-        for (int t = 0; t < T; t++) {
+        
+        for(int i = 0; i < T; i++) {
             String str = br.readLine();
             Matcher matcher = pattern.matcher(str);
-
-            if (matcher.matches()) {
+            
+            if(matcher.matches()) {
                 System.out.println("Infected!");
-            } else {
+            }else {
                 System.out.println("Good");
             }
         }
+       
     }
 }
