23fd936895f5a58f053ca2f8df20e4f97f983365
Minje Kim <wbsebqb1118@naver.com>
2025-08-23 20:16:57 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,32 +1,32 @@
-import java.util.*;
 import java.io.*;
 public class Main{
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int t = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < t; i++) {
             String str = br.readLine();
-            Stack st = new Stack();
-            boolean flag = true;
+            int balance = 0;
+            boolean valid = true;
+            
             for(int j = 0; j < str.length(); j++) {
                 char c = str.charAt(j);
                 if(c =='(') {
-                    st.push(c);
+                    balance++;
                 }else if(c == ')') {
-                    if(!st.isEmpty() ){
-                         st.pop();
-                    }else {
-                        flag = false;
+                    balance--;
+                    
+                    if(balance < 0) {
+                        valid = false;
+                        break;
                     }
                 }
             }
             
-            if(!st.isEmpty() || !flag) {
-                System.out.println("NO");
-            }else {
+            if(valid && balance ==0) {
                 System.out.println("YES");
+            }else {
+                System.out.println("NO");
             }
             
         }
