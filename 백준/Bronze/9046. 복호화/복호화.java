b6b044960e62678207f751befb98e03a0d842e82
Minje Kim <wbsebqb1118@naver.com>
2025-10-08 13:44:39 +0900

    [Bronze II] Title: 복호화, Time: 152 ms, Memory: 16684 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.java
+++ 백준/Bronze/9046. 복호화/복호화.java
@@ -1,48 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
-    StringBuilder sb = new StringBuilder();
-
-    for(int i = 0; i < T; i++) {
-
-      char[] c = br.readLine().toCharArray();
-      int[] num = new int[26];
-
-      for(int j = 0; j < c.length; j++) {
-
-        if(c[j] >= 'a' && c[j] <= 'z') {
-            num[c[j] - 'a'] += 1;
+    public static void main(String args[]) throws IOException{
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int a = 0; a < T; a++) {
+         //1. replaceAll해서 공백 없애고 toCharArray로 문자열 받기 
+        String str = br.readLine().replaceAll(" ", "");
+        Map<Character, Integer> charMap = new HashMap<>();
+        
+        //2. 배열 개수만큼 반복 돌리면서, 키 값에 charArray의 값 하나씩 넣고 벨류값을 + 1 혹은 default 사용 
+        int maxValue = 0;
+        for(int i = 0; i < str.length(); i++) {
+            Character c = str.charAt(i);
+            charMap.put(c , charMap.getOrDefault(c , 0) + 1);
+            
+            if(charMap.get(c) > maxValue) {
+                maxValue = charMap.get(c);
+            }
         }
-
-      }
-
-      int max = 0;
-      int n = 0;
-      boolean duplicate = false;
-
-      for(int k =0; k < num.length; k++) {
-        if(num[k] > max) {
-          max = num[k];
-          n = k;
-          duplicate = false;
-        }else if (num[k] == max) {
-          duplicate = true;
+        
+        //3. MaxValue를 0으로 두고 키 값 반복 돌리면서 빈도수 비교 
+        int maxCount = 0;
+        Character maxChar = '?';
+        for(Map.Entry<Character, Integer> entry : charMap.entrySet()) {
+            if(charMap.get(entry.getKey()) == maxValue) {
+                maxCount++;
+                maxChar = entry.getKey();
+            } 
         }
-      }
-
-      if(duplicate) {
-        sb.append("?");
-      }else {
-        sb.append((char)(n + 97));
-      }
-       sb.append("\n");
+        
+       
+        //4. 비교해서 나온 Max 키값 저장하고 그 값이면
+        if(maxCount > 1) {
+            System.out.println('?');
+        }else {
+             System.out.println(maxChar);
+        }
+       }
+        
+        
     }
-
-    System.out.println(sb);
-
-  }
 }
