bf3c005d0d95f387bdda9a11b444b37ddcce4a99
Minje Kim <wbsebqb1118@naver.com>
2025-11-04 20:19:28 +0900

    [Silver IV] Title: 큐 2, Time: 1252 ms, Memory: 360840 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,60 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int N = Integer.parseInt(br.readLine()); //한 줄 읽음. 15라는 숫자
-      Deque<Integer> q = new LinkedList<>();
-      StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> dq = new ArrayDeque<>();
 
-      //N수 만큼 반복돌기
-      for(int i = 0; i < N; i++) {
-        String[] Arr = br.readLine().split(" ");
+        int N = Integer.parseInt(br.readLine());
 
-        switch(Arr[0]) {
-          case "push" :
-            q.add(Integer.parseInt(Arr[1]));
-            break;
-          case "pop"  :
-            Integer n = q.poll();
-            if(n == null) {
-              sb.append(-1).append('\n');
-            }else {
-              sb.append(n).append('\n');
+        for (int i = 0; i < N; i++) {
+            String[] arr = br.readLine().split(" ");
+            String order = arr[0];
+
+            switch (order) {
+                case "push":
+                    dq.offerLast(Integer.parseInt(arr[1]));
+                    break;
+
+                case "pop":
+                    sb.append(dq.isEmpty() ? -1 : dq.pollFirst()).append('\n');
+                    break;
+
+                case "size":
+                    sb.append(dq.size()).append('\n');
+                    break;
+
+                case "empty":
+                    sb.append(dq.isEmpty() ? 1 : 0).append('\n');
+                    break;
+
+                case "front":
+                    sb.append(dq.isEmpty() ? -1 : dq.peekFirst()).append('\n');
+                    break;
+
+                case "back":
+                    sb.append(dq.isEmpty() ? -1 : dq.peekLast()).append('\n');
+                    break;
             }
-            break;
-          case "size" :
-            sb.append(q.size()).append('\n');
-            break;
-          case "empty" :
-            if(q.size() == 0) {
-              sb.append(1).append('\n');
-            }else {
-              sb.append(0).append('\n');
-            }
-            break;
-          case "front" :
-             Integer n2 = q.peek();
-             if(n2 == null) {
-               sb.append(-1).append('\n');
-             }else {
-               sb.append(n2).append('\n');
-             }
-             break;
-          case "back" :
-            Integer n3 = q.peekLast();
-            if(n3 == null) {
-              sb.append(-1).append('\n');
-            }else {
-              sb.append(n3).append('\n');
-            }
-            break;
         }
-      }
-        System.out.println(sb);
-  }
+
+        System.out.print(sb);
+    }
 }
